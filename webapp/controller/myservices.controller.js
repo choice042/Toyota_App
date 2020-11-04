ed240174ed2fa7d9398f93495826d551a6c20577
sarath809605 <sarathkumar.merangi@gmail.com>
2020-11-04 11:17:27 +0000

    my services view added

--- webapp/controller/myservices.controller.js
+++ webapp/controller/myservices.controller.js
@@ -7,13 +7,37 @@ sap.ui.define([
 	return Controller.extend("inc.demo.Toyota.controller.myservices", {
 
 		onInit: function () {
-
+			this.onOngoingPress();
 		},
 		getRouter: function () {
 			return UIComponent.getRouterFor(this);
 		},
 		onClose: function () {
 			this.getRouter().navTo("Home");
+		},
+		onOngoingPress: function () {
+			this.getView().byId("ongoing").addStyleClass("myservicesBtnClick");
+			this.getView().byId("past").addStyleClass("myservicesBtnnotClick");
+			this.getView().byId("upcoming").addStyleClass("myservicesBtnnotClick");
+			this.getView().byId("ongoing").removeStyleClass("myservicesBtnnotClick");
+			this.getView().byId("past").removeStyleClass("myservicesBtnClick");
+			this.getView().byId("upcoming").removeStyleClass("myservicesBtnClick");
+		},
+		onPastPress: function () {
+			this.getView().byId("ongoing").removeStyleClass("myservicesBtnClick");
+			this.getView().byId("past").addStyleClass("myservicesBtnClick");
+			this.getView().byId("upcoming").addStyleClass("myservicesBtnnotClick");
+			this.getView().byId("ongoing").addStyleClass("myservicesBtnnotClick");
+			this.getView().byId("past").removeStyleClass("myservicesBtnnotClick");
+			this.getView().byId("upcoming").removeStyleClass("myservicesBtnClick");
+		},
+		onUpcomingPress: function () {
+			this.getView().byId("ongoing").removeStyleClass("myservicesBtnClick");
+			this.getView().byId("past").addStyleClass("myservicesBtnnotClick");
+			this.getView().byId("upcoming").removeStyleClass("myservicesBtnnotClick");
+			this.getView().byId("ongoing").addStyleClass("myservicesBtnnotClick");
+			this.getView().byId("past").removeStyleClass("myservicesBtnClick");
+			this.getView().byId("upcoming").addStyleClass("myservicesBtnClick");
 		}
 
 	});
