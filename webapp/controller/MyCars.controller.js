cc62c639f5d1256a3ce7f1d40a746005b5556a41
choice042 <rishabhkashyap042@gmail.com>
2020-11-09 10:46:46 +0000

    new commit

--- webapp/controller/MyCars.controller.js
+++ webapp/controller/MyCars.controller.js
@@ -12,35 +12,29 @@ sap.ui.define([
              oDataGlobalModel.loadData("model/jsonFile.json", null, false);
 			 this.oDataGlobalModel = oDataGlobalModel;
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				this.oRouter.getRoute("MyCars");
 		},
 		onNavBack: function () {
 				this.oRouter.navTo("Home");
-			}
-			/**
-			 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-			 * (NOT before the first rendering! onInit() is used for that one!).
-			 * @memberOf inc.demo.Toyota.view.MyCars
-			 */
-			//	onBeforeRendering: function() {
-			//
-			//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf inc.demo.Toyota.view.MyCars
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf inc.demo.Toyota.view.MyCars
-		 */
-		//	onExit: function() {
-		//
-		//	}
+			},
+				onServiceStatusPress: function () {
+			this.oRouter.navTo("ServiceStatus");
+		},
+		onProductPress: function () {
+			this.oRouter.navTo("Products");
+		},
+		onPressConfirm: function () {
+			this.oRouter.navTo("confirmAppointment");
+		},
+		onMyServicesPress: function () {
+			this.oRouter.navTo("myservices");
+		},
+		onBookservicePress: function () {
+			this.oRouter.navTo("BookingScreen");
+		},
+		onPressHome: function(){
+				this.oRouter.navTo("Home");
+		}
 
 	});
 
--- webapp/controller/Products.controller.js
+++ webapp/controller/Products.controller.js
@@ -20,7 +20,45 @@ sap.ui.define([
 			getRouter: function () {
 			return UIComponent.getRouterFor(this);
 		},
-			onServiceStatusPress: function () {
+			onHomePress: function () {
+			this.getView().byId("home").addStyleClass("redFooter");
+			this.getView().byId("mycar").removeStyleClass("redFooter");
+			this.getView().byId("products").removeStyleClass("redFooter");
+			this.getView().byId("bookings").removeStyleClass("redFooter");
+			this.getView().byId("more").removeStyleClass("redFooter");
+		},
+		onMyCarPress: function () {
+			this.getView().byId("mycar").addStyleClass("redFooter");
+			this.getView().byId("home").removeStyleClass("redFooter");
+			this.getView().byId("products").removeStyleClass("redFooter");
+			this.getView().byId("bookings").removeStyleClass("redFooter");
+			this.getView().byId("more").removeStyleClass("redFooter");
+		},
+		onProductPress: function () {
+			this.getView().byId("products").addStyleClass("redFooter");
+			this.getView().byId("mycar").removeStyleClass("redFooter");
+			this.getView().byId("home").removeStyleClass("redFooter");
+			this.getView().byId("bookings").removeStyleClass("redFooter");
+			this.getView().byId("more").removeStyleClass("redFooter");
+		},
+		onBookingsPress: function () {
+			this.getView().byId("bookings").addStyleClass("redFooter");
+			this.getView().byId("mycar").removeStyleClass("redFooter");
+			this.getView().byId("products").removeStyleClass("redFooter");
+			this.getView().byId("home").removeStyleClass("redFooter");
+			this.getView().byId("more").removeStyleClass("redFooter");
+		},
+		onMorePress: function () {
+			this.getView().byId("more").addStyleClass("redFooter");
+			this.getView().byId("mycar").removeStyleClass("redFooter");
+			this.getView().byId("products").removeStyleClass("redFooter");
+			this.getView().byId("bookings").removeStyleClass("redFooter");
+			this.getView().byId("home").removeStyleClass("redFooter");
+		},
+		onNotificationPress: function () {
+			this.oRouter.navTo("Notifications");
+		},
+		onServiceStatusPress: function () {
 			this.oRouter.navTo("ServiceStatus");
 		},
 		onProductPress: function () {
@@ -35,8 +73,21 @@ sap.ui.define([
 		onBookservicePress: function () {
 			this.oRouter.navTo("BookingScreen");
 		},
-		onPressHome: function(){
-				this.oRouter.navTo("Home");
+		onMyCarPress: function () {
+			this.oRouter.navTo("MyCars");
+		},
+		onClose: function () {
+			this.oRouter.navTo("Home");
+		},
+		onUserPress: function () {
+			/*var that = this;
+			if (!this.userDetails) {
+				this.userDetails = sap.ui.xmlfragment("ggg","inc.demo.Toyota.fragment.User", this);
+			}
+			this.getView().addDependent(this.userDetails);*/
+			// this.userDetails.open();
+			// console.log("sarath");
+			this.oRouter.navTo("myProfile");
 		}
 
 	
